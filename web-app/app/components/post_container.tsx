65802e63dbd6de8c40fe658a58464dfdc415d865
Pablo Yuri <pabloyuridev@gmail.com>
2025-02-03 04:06:18 -0300

    adiciona o componente post container

--- web-app/app/components/post_container.tsx
+++ web-app/app/components/post_container.tsx
@@ -1,3 +1,18 @@
-export default function PostContainer() {
-  return <div></div>; // BACK BUTTON WITH CALLBACK AND SIMPLE TEXT ELEMENT
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/ArrowBack";
+
+export default function PostContainer(props: { backCallback: () => void }) {
+  return (
+    <section className="flex flex-col items-start gap-4 w-[800px]">
+      <Button
+        onClick={() => props.backCallback()}
+        variant="text"
+        startIcon={<DeleteIcon />}
+      >
+        Voltar
+      </Button>
+      <p className="text-2xl font-bold text-zinc-50">Post Title</p>
+      <p className="text-zinc-300">Post Content</p>
+    </section>
+  );
 }
