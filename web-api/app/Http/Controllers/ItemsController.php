fac55f77685c2314cada750d2f55b151c032a40b
Pablo Yuri <pabloyuridev@gmail.com>
2025-02-03 04:53:21 -0300

    adiciona o controlador de items

---
+++ web-api/app/Http/Controllers/ItemsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+use Faker\Factory as Faker;
+
+class ItemsController extends Controller {
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255'
+        ]);
+
+        $faker = Faker::create();
+        $loremIpsum = $faker->paragraph; 
+        $item = Item::create([
+            'title' => $validated['title'],
+            'body'  => $loremIpsum
+        ]);
+
+        return response()->json($item, 201);
+    }
+
+    public function showByTitle($title) {
+        $items = Item::where('title', 'like', '%' . $title . '%')
+            ->limit(20)
+            ->get();
+
+        if ($items->isEmpty()) {
+            return response()->json(['message' => 'Post não encontrado'], 404);
+        }
+        return response()->json($items);
+    }
+}
