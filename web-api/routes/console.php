4b3e581749cbcb617aa5cd9585f96fb423a1d473
Pablo Yuri <pabloyuridev@gmail.com>
2025-02-04 03:44:34 -0300

    implementa o SyncItemsJob

--- web-api/routes/console.php
+++ web-api/routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
+
+use App\Jobs\SyncItemsJob;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('sync:items', function () {
+    dispatch(new SyncItemsJob);
+    $this->info('SyncItemsJob dispatched successfully.');
+})->purpose('Dispatch the SyncItemsJob.');
+
+$schedule = app(Schedule::class);
+$schedule->command('sync:items')->daily();
