2f9b68660d593fc04d90d5fc1d431c6b0b7f3868
Pablo Yuri <pabloyuridev@gmail.com>
2025-02-05 04:00:20 -0300

    configura a responsividade da aplicação web

--- web-app/app/components/post_container.tsx
+++ web-app/app/components/post_container.tsx
@@ -3,7 +3,7 @@ import DeleteIcon from "@mui/icons-material/ArrowBack";
 
 export default function PostContainer(props: { backCallback: () => void }) {
   return (
-    <section className="flex flex-col items-start gap-4 w-[800px]">
+    <section className="flex flex-col items-start gap-4 w-full p-3 max-w-[800px]">
       <Button
         onClick={() => props.backCallback()}
         variant="text"
--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -12,19 +12,19 @@ export default function Home() {
   return (
     <div className="flex flex-1 min-h-screen items-center justify-center bg-gradient-to-br from-zinc-800 to-zinc-950">
       {!showPost && (
-        <section className="flex flex-row gap-10">
+        <section className="flex flex-col lg:items-center lg:flex-row gap-10 p-3">
           <div className="flex flex-col gap-4">
-            <p className="text-zinc-50 font-medium text-4xl tracking-tight">
+            <p className="text-zinc-50 font-medium text-2xl sm:text-3xl tracking-tight">
               Encontre os posts mais relevantes
             </p>
-            <p className="text-zinc-400 text-lg tracking-tight max-w-xl">
+            <p className="text-zinc-400 text-md sm:text-lg tracking-tight">
               Na <span className="font-bold">Postlee</span>, você tem acesso a
               uma variedade de posts excepcionais ou pode{" "}
               <span className="font-medium">contribuir com o seu próprio</span>!
             </p>
           </div>
 
-          <div className="flex flex-col gap-5 w-96">
+          <div className="flex flex-col gap-5 w-full max-w-96">
             <Autocomplete
               disablePortal
               options={[]}
